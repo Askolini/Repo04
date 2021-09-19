8737370a549cdbceed895e1120fea560530e57f1
Askolini <asmircc@gmail.com>
2021-09-19 05:37:07 +0200

    String Litteral

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main (){
+
+    //
+    var myFirstName = "Asmir";
+    var myLastName = "Ćatić";
+    var myHomeTown = "Sarajevo";
+
+    //
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+
+module.exports = main;
