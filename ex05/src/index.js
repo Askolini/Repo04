959a4990d5fbca84d2c8c4525dcda6abd510bf75
Askolini <asmircc@gmail.com>
2021-09-19 04:57:57 +0200

    Multiply

---
+++ ex05/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    //
+    var numOne;
+    var numTwo;
+    var product = numOne * numTwo;
+    //
+
+    return product;
+}
+console.log(main(2.5, 2.0));
+console.log(main(5.5, 5.5));
+console.log(main(100.25, 43.20));
+
+module.exports = main;
