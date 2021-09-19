fc31e39220c1ada299a5fbbeeb0f0c8187b2a3d6
Askolini <asmircc@gmail.com>
2021-09-19 04:51:00 +0200

    Decimal

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+function main (){
+
+        //
+        var myDecimal = 8.2
+        var myDecimalE = 0.00254
+        //
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+
+module.exports = main;
