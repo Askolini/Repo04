a96a59a3b4480152e4887790fda75ddaced8fb04
Askolini <asmircc@gmail.com>
2021-09-19 05:05:56 +0200

    Divide Decimal

---
+++ ex06/src/index.js
@@ -0,0 +1,15 @@
+function main (numOne, numTwo){
+
+        //
+        var numOne;
+        var numTwo;
+        var quotient = numOne / numTwo;
+        //
+        return quotient;
+}
+
+console.log(main(5.0, 2.0));
+console.log(main(16.5, 5.5));
+console.log(main(102.0, 25.5));
+
+module.exports = main;
